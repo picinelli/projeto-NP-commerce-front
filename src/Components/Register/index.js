3af1cee15e3a609da91af582aaa13de9c03855e1
Pedro Ivo <cinelli.dev@gmail.com>
2022-05-11 18:07:30 -0300

    feat. register page is now done and functional

--- src/Components/Register/index.js
+++ src/Components/Register/index.js
@@ -1,7 +1,21 @@
-import styled from 'styled-components'
-import Logo from '../../Assets/images/Logo.jpg'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+import { ThreeDots } from "react-loader-spinner";
+
+import Logo from "../../Assets/images/Logo.jpg";
 
 export default function Register() {
+  const navigate = useNavigate();
+  const [disabled, setDisabled] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+  });
+
   return (
     <Container>
       <Header>
@@ -9,50 +23,118 @@ export default function Register() {
       </Header>
       <Wrapper>
         <h1>Cadastro</h1>
-        <Form>
-          <Input placeholder="Nome" />
-          <Input placeholder="Email" />
-          <Input placeholder="Senha"/>
-          <Input placeholder="Confirme a senha"/>
-          <RegisterButton>Cadastrar!</RegisterButton>
-          <LoginButton>Já tem uma conta? <span>Entre agora!</span></LoginButton>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            placeholder="Nome"
+            type="text"
+            value={user.name}
+            onChange={(e) => {
+              setUser({ ...user, name: e.target.value });
+            }}
+            required
+          />
+          <Input
+            placeholder="Email"
+            type="email"
+            value={user.email}
+            onChange={(e) => {
+              setUser({ ...user, email: e.target.value });
+            }}
+            required
+          />
+          <Input
+            placeholder="Senha"
+            type="password"
+            value={user.password}
+            onChange={(e) => {
+              setUser({ ...user, password: e.target.value });
+            }}
+            required
+          />
+          <Input
+            placeholder="Confirme a senha"
+            type="password"
+            value={user.confirm}
+            onChange={(e) => {
+              setUser({ ...user, confirm: e.target.value });
+            }}
+            required
+          />
+          <LoadButtons />
         </Form>
       </Wrapper>
     </Container>
-  )
+  );
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setDisabled(true);
+    try {
+      await axios.post("http://localhost:5000/register", user);
+      setDisabled(false);
+      navigate("/");
+    } catch (e) {
+      setDisabled(false);
+      window.alert(e);
+      console.log(e);
+    }
+  }
+
+  function LoadButtons() {
+    if (disabled === true) {
+      return (
+        <>
+          <RegisterButton disabled>
+            <ThreeDots color="#2728EF" />
+          </RegisterButton>
+          <LoginButton disabled>
+            Já tem uma conta? <span>Entre agora!</span>
+          </LoginButton>
+        </>
+      );
+    }
+    return (
+      <>
+        <RegisterButton type="submit">Cadastrar!</RegisterButton>
+        <LoginButton>
+          Já tem uma conta? <span>Entre agora!</span>
+        </LoginButton>
+      </>
+    );
+  }
 }
 
 const Container = styled.div`
-  background-color: #EDEDED;
+  background-color: #ededed;
   width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-family: 'Roboto', sans-serif;
-`
+  font-family: "Roboto", sans-serif;
+`;
 
 const Header = styled.header`
   height: 80px;
   width: 100vw;
-  background-color: #FFF159;
+  background-color: #fff159;
   display: flex;
   align-items: center;
   justify-content: center;
-  -webkit-box-shadow: 0px 0px 16px 2px rgba(0,0,0,0.43); 
-  box-shadow: 0px 0px 16px 2px rgba(0,0,0,0.43);
+  -webkit-box-shadow: 0px 0px 16px 2px rgba(0, 0, 0, 0.43);
+  box-shadow: 0px 0px 16px 2px rgba(0, 0, 0, 0.43);
 
   img {
     width: 180px;
     height: 60px;
     object-fit: cover;
   }
-`
+`;
 
 const Wrapper = styled.div`
   margin: auto;
   padding: 30px;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   max-width: 380px;
   width: 100%;
   border-radius: 5px;
@@ -62,18 +144,18 @@ const Wrapper = styled.div`
     font-size: 1.5rem;
     padding-bottom: 50px;
   }
-`
+`;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+`;
 
 const Input = styled.input`
   border: 0;
   border-radius: 3px;
-  background-color: #EFEFEF;
+  background-color: #efefef;
   width: 100%;
   height: 40px;
   margin-bottom: 30px;
@@ -81,25 +163,29 @@ const Input = styled.input`
   font-size: 16px;
 
   ::placeholder {
-  font-size: 16px;
-  color: #666666;
-  padding-left: 4px;
-  /* position: absolute;
+    font-size: 16px;
+    color: #666666;
+    padding-left: 4px;
+    /* position: absolute;
   left: 10px;
   top: 30%; */
-}
-`
+  }
+`;
 
 const RegisterButton = styled.button`
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   font-size: 18px;
   border: 0;
   border-radius: 35px;
-  background-color: #FFF159;
+  background-color: #fff159;
   width: 150px;
   height: 50px;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
-`
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+`;
 
 const LoginButton = styled.button`
   cursor: pointer;
@@ -110,4 +196,4 @@ const LoginButton = styled.button`
   span {
     color: blue;
   }
-`
+`;
