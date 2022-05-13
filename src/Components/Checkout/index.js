046a52cfe77a67ad2b2d987ea9f7edc561ee4554
Natan__Brito <natanismaelbrito@gmail.com>
2022-05-12 22:03:46 -0300

    design finishing

--- src/Components/Checkout/index.js
+++ src/Components/Checkout/index.js
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../Assets/images/Logo.jpg";
 import { ThreeDots } from "react-loader-spinner";
 export default function Checkout() {
+  const test="testinho"
   const dog="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgolBdeaXdt7hZ4G28YiA8shOCg4jkBg08uA&usqp=CAU"
   return (
     <>
@@ -13,48 +14,100 @@ export default function Checkout() {
       <img src={Logo} alt="Logo" />
       </Header>
       <AllProducts>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
-      <ItemProducts img={dog}/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
+      <ItemProducts img={dog} text="gatin reverse" price="15.75"/>
       
       </AllProducts>
       <Footer>
+        <Price>
+          <span><h1>Subtotal:  </h1>  <h1>R$ 15.90</h1></span>
+        </Price>
+      <button>
+        Finalizar
+      </button>
       </Footer>
     </Container>
     </>
   )
-  function ItemProducts({img}){
+  function ItemProducts({img,text,price}){
+    const randomNumber=Math.random() * (-1 - 0) + 1;
+    const priceDescont=price*(1-randomNumber);
    return(
     <Products>
       <div className="leftImgName">
       <img src={img} alt="doguinho"/>
-      <h1>catiorroooooosssssssssssssssssssssssssssooooo</h1> 
+      <h1>{text}</h1> 
       </div>
       <div className="rightPrice">
-      <span>R$15.90</span>
+      <span className=""><s className="green">%{(randomNumber*100).toFixed(0)}</s><s className="risco">R${price}</s></span>
+      <span className="duoSpan">R${ priceDescont.toFixed(2)}</span>
+
       </div>
     </Products>
    )
   }
 }
-const Footer = styled.header`
+const Price= styled.div`
+display: flex;
+justify-content: space-between;
+padding: 10px;
+max-width: 100%;
+width: 91%;
+height: 32px;
+margin: 1px auto;
+span{
+  display: flex;
+  font-size: 25px;
+    font-weight: bold;
+}
+h1{
+  margin-right: 10px;
+  font-family: "Roboto", sans-serif;
+    font-size: 90%;
+    font-weight: bold;
+}
+`
+const Footer = styled.footer`
   height: 100px;
   width: 100vw;
   background-color: #FFFFFF;
-  display: flex;
   position: fixed;
   bottom: 0;
   /* align-items: center;
   justify-content: center; */
   -webkit-box-shadow: 0px 0px 16px 2px rgba(0, 0, 0, 0.43);
   box-shadow: 0px 0px 16px 2px rgba(0, 0, 0, 0.43);
+  button{
+    display: flex;
+    margin: auto;
+    min-width: 100px;
+    max-width: 300px;
+    width: 100%;
+    min-height: 20px;
+    height: 50px;
+    border-radius: 25px;
+    border:none;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+    font-weight: 400;
+    font-family: "Roboto", sans-serif;
+    background-color: #FFF159;
+    cursor: pointer;
+    
+  }
+  button:hover{
+      transition: 0.8s;
+      background-color: #f3df09;
+    }
 
   img {
     width: 180px;
@@ -90,12 +143,11 @@ const Products = styled.div`
   }
   h1{
     max-width:100%;
-    /* width: 100%; */
-    background-color: aqua;
+    /* background-color: aqua; */
     font-family: "Roboto", sans-serif;
     margin-top:5px;
     margin-left: 15px;
-    font-size: 25px;
+    font-size: 100%;
     font-weight: bold;
     overflow: hidden;
     
@@ -105,12 +157,21 @@ const Products = styled.div`
   }
   .rightPrice{
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     margin-right: 50px;
   }
-  span{
-    font-size: 30px;
+  .green{
+    color:green;
+    font-weight: bold;
+  }
+  .risco{
+    text-decoration: line-through;
+    margin-left:5px;
+}
+  .duoSpan{
+    font-size: 100%;
     font-weight: bold;
     font-family: "Roboto", sans-serif;
   }
